316e38cfe5aa68192fa141542a3d4493f4868ad1
Федор Жеков <zhekovfi@gmail.com>
2020-09-21 07:26:02 +0000

    Feature/nuke plugin

---
+++ src/PikTools.Nuke/PikToolsBuild.cs
@@ -0,0 +1,81 @@
+﻿namespace PikTools.Nuke
+{
+    using System.Linq;
+    using global::Nuke.Common;
+    using global::Nuke.Common.ProjectModel;
+    using global::Nuke.Common.Utilities;
+    using JetBrains.Annotations;
+
+    /// <summary>
+    /// Расширение билд скрипта для сборки MSI
+    /// </summary>
+    [PublicAPI]
+    public abstract class PikToolsBuild : NukeBuild
+    {
+        private readonly Wix _wix;
+        private Project _project;
+        private string _config;
+
+        /// <summary>
+        /// ctor
+        /// </summary>
+        protected PikToolsBuild()
+        {
+            _wix = new Wix();
+        }
+
+        /// <summary>
+        /// Solution
+        /// </summary>
+        [Solution]
+        public Solution Solution { get; set; }
+
+        /// <summary>
+        /// Project
+        /// </summary>
+        [Parameter("Select project")]
+        public Project Project
+        {
+            get
+            {
+                if (_project == null)
+                {
+                    var result = ConsoleUtility.PromptForChoice(
+                        "Select project:",
+                        Solution.AllProjects
+                            .Select(x => (x.Name, x.Name))
+                            .ToArray());
+                    _project = Solution.AllProjects.FirstOrDefault(x => x.Name == result);
+                }
+
+                return _project;
+            }
+            set => _project = value;
+        }
+
+        /// <summary>
+        /// BuildMsi
+        /// </summary>
+        public Target BuildMsi => _ => _
+            .Description("Собирает MSi пакет из указанного проекта")
+            .Requires(() => Project)
+            .Requires(() => Config)
+            .Executes(() => { _wix.BuildMsi(Project, Config); });
+
+        /// <summary>
+        /// Генерирует необходимые свойства в проекте
+        /// </summary>
+        public Target GenerateProjectProps => _ => _
+            .Requires(() => Project)
+            .Requires(() => Config)
+            .Executes(() => new ProjectPropertriesGenerator().GenerateProperties(Project, Config));
+
+        [Parameter("Select configuration")]
+        private string Config
+        {
+            get => _config ??=
+                ConsoleUtility.PromptForChoice("Select config:", ("Debug", "Debug"), ("Release", "Release"));
+            set => _config = value;
+        }
+    }
+}
---
+++ src/PikTools.Nuke/StringExtensions.cs
@@ -0,0 +1,35 @@
+﻿namespace PikTools.Nuke
+{
+    using System;
+    using Application.Api;
+    using Command.Api;
+
+    /// <summary>
+    /// Расширения для строк
+    /// </summary>
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Имя типа в <see cref="PluginType"/>
+        /// </summary>
+        /// <param name="type">имя типа</param>
+        public static PluginType ToPluginType(this string type)
+        {
+            return type switch
+            {
+                nameof(PikToolsCommand) => PluginType.Command,
+                nameof(PikToolsApplication) => PluginType.Application,
+                _ => throw new NotSupportedException()
+            };
+        }
+
+        /// <summary>
+        /// Преобразует строку в имя свойства для csproj
+        /// </summary>
+        /// <param name="str">исходная строка</param>
+        public static string ToPropertyName(this string str)
+        {
+            return str.Replace(".", "_");
+        }
+    }
+}
