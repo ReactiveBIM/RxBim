d7c7e880c471ba76f6ce94a76b38c8d77cd111b2
Александр Пекшев <pekshevaa@pik.ru>
2020-09-29 12:58:16 +0000

    Сервис работы с пользовательскими настройками
    - Сервис создает в папке %appdata%\PIK Tools файл с именем namespace вызывающей сборки и с расширением .settings
    - Сервис позволяет сохранять экземпляры классов (должны обладать публичным безпараметрическим конструктором и публичными полями), а также коллекции. Для хранения коллекций требуется указывать имя узла, так как тип коллекции не обладает уникальным именем
    - При чтении настроек происходит создание экземпляра объекта заданного типа если настройки не найдены в файле или не удалось их прочитать

---
+++ examples/PikTools.Command.Example/Cmd3.cs
@@ -0,0 +1,52 @@
+﻿namespace PikTools.CommandExample
+{
+    using System.Collections.Generic;
+    using Autodesk.Revit.Attributes;
+    using Command.Api;
+    using Models;
+    using Shared;
+    using Shared.Abstractions;
+    using Shared.Ui.Abstractions;
+
+    /// <summary>
+    /// Тестовая команда проверки работы с сервисом пользовательских настроек
+    /// </summary>
+    [Regeneration(RegenerationOption.Manual)]
+    [Transaction(TransactionMode.Manual)]
+    public class Cmd3 : PikToolsCommand
+    {
+        /// <summary>
+        /// Запуск тестовой команды
+        /// </summary>
+        /// <param name="userSettings">Сервис работы с пользовательскими настройками</param>
+        /// <param name="notificationService">Сервис уведомлений</param>
+        public PluginResult ExecuteCommand(IUserSettings userSettings, INotificationService notificationService)
+        {
+            var mySettings = new MySettings
+            {
+                StringValue = "Hello world",
+                IntValue = 12,
+                DoubleValue = 0.144
+            };
+
+            userSettings.Set(mySettings);
+            notificationService.ShowMessage(nameof(Cmd3), "Настройки сохранены");
+
+            var loaded = userSettings.Get<MySettings>();
+            notificationService.ShowMessage(nameof(Cmd3), $"Настройки загружены со значениями: {nameof(loaded.StringValue)}: {loaded.StringValue}; {nameof(loaded.IntValue)} : {loaded.IntValue}; {nameof(loaded.DoubleValue)}: {loaded.DoubleValue}");
+
+            var list = new List<string>
+            {
+                "Value 1", "Value 2", "Value 3"
+            };
+
+            userSettings.Set(list, nodeName: "MyList");
+            notificationService.ShowMessage(nameof(Cmd3), "В настройки сохранена коллекция строк с именем MyList");
+
+            var loadedList = userSettings.Get<List<string>>("MyList");
+            notificationService.ShowMessage(nameof(Cmd3), $"Из настроек загружена коллекция со значениями: {string.Join(", ", loadedList)}");
+
+            return PluginResult.Succeeded;
+        }
+    }
+}
---
+++ src/PikTools.Shared/ContainerExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace PikTools.Shared
+{
+    using Abstractions;
+    using SimpleInjector;
+
+    /// <summary>
+    /// Расширения для контейнера
+    /// </summary>
+    public static class ContainerExtensions
+    {
+        /// <summary>
+        /// Добавляет общие вспомогательные сервисы
+        /// </summary>
+        /// <param name="container">DI контейнер</param>
+        public static void AddSharedTools(this Container container)
+        {
+            container.Register<IUserSettings, UserSettings>(Lifestyle.Singleton);
+        }
+    }
+}
