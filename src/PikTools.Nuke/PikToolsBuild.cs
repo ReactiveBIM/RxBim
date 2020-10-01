d56e20b4658729595285a90753afdda7277a2d17
Федор Жеков <zhekovfi@gmail.com>
2020-10-01 14:36:31 +0300

    fix

--- src/PikTools.Nuke/PikToolsBuild.cs
+++ src/PikTools.Nuke/PikToolsBuild.cs
@@ -1,9 +1,12 @@
 ﻿namespace PikTools.Nuke
 {
+    using System;
     using System.Linq;
+    using System.Text.RegularExpressions;
     using Generators;
     using global::Nuke.Common;
     using global::Nuke.Common.ProjectModel;
+    using global::Nuke.Common.Tools.Git;
     using global::Nuke.Common.Utilities;
     using JetBrains.Annotations;
 
@@ -14,8 +17,9 @@
     public abstract class PikToolsBuild : NukeBuild
     {
         private readonly Wix _wix;
-        private Project _project;
+        private string _project;
         private string _config;
+        private Regex _releaseBranchRegex;
 
         /// <summary>
         /// ctor
@@ -35,7 +39,7 @@
         /// Project
         /// </summary>
         [Parameter("Select project")]
-        public Project Project
+        public string Project
         {
             get
             {
@@ -46,7 +50,7 @@
                         Solution.AllProjects
                             .Select(x => (x.Name, x.Name))
                             .ToArray());
-                    _project = Solution.AllProjects.FirstOrDefault(x => x.Name == result);
+                    _project = Solution.AllProjects.FirstOrDefault(x => x.Name == result)?.Name;
                 }
 
                 return _project;
@@ -61,7 +65,46 @@
             .Description("Собирает MSi пакет из указанного проекта")
             .Requires(() => Project)
             .Requires(() => Config)
-            .Executes(() => { _wix.BuildMsi(Project, Config); });
+            .Executes(() => { _wix.BuildMsi(ProjectForMsiBuild, Config); });
+
+        /// <summary>
+        /// Проверяет текущюю ветку
+        /// </summary>
+        public Target CheckCurrentBranch => _ => _
+            .Executes(() =>
+            {
+                var gitCurrentBranch = GitTasks.GitCurrentBranch();
+                _releaseBranchRegex = new Regex("release/(?<version>[0-9.]*)");
+                if (!_releaseBranchRegex.IsMatch(gitCurrentBranch))
+                {
+                    throw new ArgumentException("Current branch name is not valid!!!");
+                }
+            });
+
+        /// <summary>
+        /// Проверяет текущюю версию проектв и версию релиза
+        /// </summary>
+        public Target CheckVersion => _ => _
+            .DependsOn(CheckCurrentBranch)
+            .Executes(() =>
+            {
+                var gitCurrentBranch = GitTasks.GitCurrentBranch();
+                var gitVersion = _releaseBranchRegex.Match(gitCurrentBranch).Groups["version"].Value;
+                var projectVersion = ProjectForMsiBuild.GetProperty("Version");
+                if (gitVersion != projectVersion)
+                {
+                    throw new ArgumentException("Project version is not equals git version!!!");
+                }
+            });
+
+        /// <summary>
+        /// Собирает msi для тестирования
+        /// </summary>
+        public Target BuildMsiForTesting => _ => _
+            .Requires(() => Project)
+            .DependsOn(CheckVersion)
+            .Executes(() => { Config = "Debug"; })
+            .Triggers(BuildMsi);
 
         /// <summary>
         /// Генерирует необходимые свойства в проекте
@@ -69,14 +112,19 @@
         public Target GenerateProjectProps => _ => _
             .Requires(() => Project)
             .Requires(() => Config)
-            .Executes(() => new ProjectPropertiesGenerator().GenerateProperties(Project, Config));
+            .Executes(() => new ProjectPropertiesGenerator().GenerateProperties(ProjectForMsiBuild, Config));
 
+        /// <summary>
+        /// Конфигурация
+        /// </summary>
         [Parameter("Select configuration")]
-        private string Config
+        public string Config
         {
             get => _config ??=
                 ConsoleUtility.PromptForChoice("Select config:", ("Debug", "Debug"), ("Release", "Release"));
             set => _config = value;
         }
+
+        private Project ProjectForMsiBuild => Solution.AllProjects.FirstOrDefault(x => x.Name == _project);
     }
 }
