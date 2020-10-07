e230334e2ef6d1129d97587f9c4fd990a7a5bc17
Modis <cjmodis@gmail.com>
2020-10-07 11:33:30 +0300

    Добавил PickLinkedElement и PickLinkedElements

--- src/PikTools.Shared.RevitExtensions/Abstractions/IElementsCollector.cs
+++ src/PikTools.Shared.RevitExtensions/Abstractions/IElementsCollector.cs
@@ -3,7 +3,6 @@
     using System;
     using System.Collections.Generic;
     using Autodesk.Revit.DB;
-    using Autodesk.Revit.UI.Selection;
 
     /// <summary>
     /// Интерфейс коллектора элементов
@@ -13,36 +12,44 @@
         /// <summary>
         /// Получить отфильтрованную коллекцию элементов
         /// </summary>
-        /// <param name="doc">Текущий документ</param>
+        /// <param name="doc">Документ для обработки. Если null, то будет браться текущий документ</param>
         /// <param name="ignoreScope">Игнорировать вариант частей <see cref="ScopeType"/></param>
         /// <param name="includeSubFamilies">Включить в коллектор вложенные семейства.
         /// Опция актуальна при <see cref="ScopeType"/> == SelectedElements</param>
         /// <returns>Отфильтрованная коллекция элементов</returns>
         FilteredElementCollector GetFilteredElementCollector(
-            Document doc, bool ignoreScope = false, bool includeSubFamilies = true);
+            Document doc = null, bool ignoreScope = false, bool includeSubFamilies = true);
 
         /// <summary>
-        /// Включает в Revit режим выбора элемента с учетом заданного фильтра
+        /// Включает в Revit режим выбора элемента в модели текущего документа с учетом заданного фильтра
         /// </summary>
         /// <param name="filterElement">Фильтр для выбора элементов</param>
         /// <param name="statusPrompt">Описание статуса в Revit при выборе</param>
-        /// <param name="objectType">Тип выбираемого объекта</param>
         /// <returns>Выбранный элемент</returns>
-        Element PickElement(
-            Func<Element, bool> filterElement = null,
-            string statusPrompt = "",
-            ObjectType objectType = ObjectType.Element);
+        Element PickElement(Func<Element, bool> filterElement = null, string statusPrompt = "");
 
         /// <summary>
-        /// Включает в Revit режим выбора элементов с учетом заданного фильтра
+        /// Включает в Revit режим выбора элементов в модели текущего документа с учетом заданного фильтра
         /// </summary>
         /// <param name="filterElement">Фильтр для выбора элементов</param>
         /// <param name="statusPrompt">Описание статуса в Revit при выборе</param>
-        /// <param name="objectType">Тип выбираемого объекта</param>
         /// <returns>Выбранный элемент</returns>
-        List<Element> PickElements(
-            Func<Element, bool> filterElement = null,
-            string statusPrompt = "",
-            ObjectType objectType = ObjectType.Element);
+        List<Element> PickElements(Func<Element, bool> filterElement = null, string statusPrompt = "");
+
+        /// <summary>
+        /// Включает в Revit режим выбора элемента в модели связанного документа с учетом заданного фильтра
+        /// </summary>
+        /// <param name="filterElement">Фильтр для выбора элементов</param>
+        /// <param name="statusPrompt">Описание статуса в Revit при выборе</param>
+        /// <returns>Выбранный элемент</returns>
+        Element PickLinkedElement(Func<Element, bool> filterElement = null, string statusPrompt = "");
+
+        /// <summary>
+        /// Включает в Revit режим выбора элементов в модели связанного документа с учетом заданного фильтра
+        /// </summary>
+        /// <param name="filterElement">Фильтр для выбора элементов</param>
+        /// <param name="statusPrompt">Описание статуса в Revit при выборе</param>
+        /// <returns>Выбранный элемент</returns>
+        List<Element> PickLinkedElements(Func<Element, bool> filterElement = null, string statusPrompt = "");
     }
 }
---
+++ src/PikTools.Shared.RevitExtensions/Helpers/LinkedElementSelectionFilter.cs
@@ -0,0 +1,44 @@
+﻿namespace PikTools.Shared.RevitExtensions.Helpers
+{
+    using System;
+    using Autodesk.Revit.DB;
+    using Autodesk.Revit.UI.Selection;
+
+    /// <inheritdoc />
+    public class LinkedElementSelectionFilter : ISelectionFilter
+    {
+        private readonly Document _doc;
+        private readonly Func<Element, bool> _filterElement;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LinkedElementSelectionFilter"/> class.
+        /// </summary>
+        /// <param name="doc">Родительский документ</param>
+        /// <param name="filterElement">Фильтр для выбора элементов</param>
+        public LinkedElementSelectionFilter(Document doc, Func<Element, bool> filterElement = null)
+        {
+            _doc = doc;
+            _filterElement = filterElement;
+        }
+
+        /// <inheritdoc />
+        public bool AllowElement(Element elem)
+        {
+            return true;
+        }
+
+        /// <inheritdoc />
+        public bool AllowReference(Reference reference, XYZ position)
+        {
+            if (_doc.GetElement(reference) is RevitLinkInstance linkInstance &&
+                reference.LinkedElementId != ElementId.InvalidElementId)
+            {
+                var docLink = linkInstance.GetLinkDocument();
+                var element = docLink.GetElement(reference.LinkedElementId);
+                return _filterElement?.Invoke(element) ?? true;
+            }
+
+            return false;
+        }
+    }
+}
