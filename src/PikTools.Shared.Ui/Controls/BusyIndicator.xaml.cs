c0f93278c2c0bea2955fbf5e55ede3f233c29266
pavlovek <pavlovek@pik.ru>
2020-10-09 17:32:28 +0300

    добавли еще стили для UI

---
+++ src/PikTools.Shared.Ui/Controls/BusyIndicator.xaml.cs
@@ -0,0 +1,50 @@
+﻿namespace PikTools.Shared.Ui.Controls
+{
+    using System.Timers;
+    using System.Windows;
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for RounderProgressBar.xaml
+    /// </summary>
+    public partial class BusyIndicator : UserControl
+    {
+        private Timer timer;
+        private bool loaded;
+
+        /// <summary>
+        /// ctor
+        /// </summary>
+        public BusyIndicator()
+        {
+            InitializeComponent();
+
+            Loaded += OnLoaded;
+        }
+
+        private delegate void VoidDelegete();
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            timer = new Timer(100);
+            timer.Elapsed += OnTimerElapsed;
+            timer.Start();
+            loaded = true;
+        }
+
+        private void OnTimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            rotationCanvas.Dispatcher.Invoke(
+                new VoidDelegete(
+                    delegate
+                        {
+                            SpinnerRotate.Angle += 30;
+                            if (SpinnerRotate.Angle == 360)
+                            {
+                                SpinnerRotate.Angle = 0;
+                            }
+                        }),
+                null);
+        }
+    }
+}
