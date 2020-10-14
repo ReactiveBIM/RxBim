95d32beee5edfa73ff873ad82600a912995d2bb7
pavlovek <pavlovek@pik.ru>
2020-10-14 15:22:23 +0300

    add wpf styles and example

---
+++ src/PikTools.Shared.Ui/Controls/BusyIndicator.cs
@@ -0,0 +1,39 @@
+﻿namespace PikTools.Shared.Ui.Controls
+{
+    using System.Windows;
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Область, для которой можно выставить индикатор ожидания
+    /// </summary>
+    public class BusyIndicator : ContentControl
+    {
+        /// <summary>
+        /// DependencyProperty for <see cref="IsBusy"/>
+        /// </summary>
+        public static readonly DependencyProperty IsBusyProperty =
+            DependencyProperty.Register(
+                nameof(IsBusy),
+                typeof(bool),
+                typeof(BusyIndicator));
+
+        /// <summary>
+        /// Initializes the <see cref="BusyIndicator"/> class.
+        /// </summary>
+        static BusyIndicator()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(
+                typeof(BusyIndicator),
+                new FrameworkPropertyMetadata(typeof(BusyIndicator)));
+        }
+
+        /// <summary>
+        /// Индикатор ожидания
+        /// </summary>
+        public bool IsBusy
+        {
+            get => (bool)GetValue(IsBusyProperty);
+            set => SetValue(IsBusyProperty, value);
+        }
+    }
+}
