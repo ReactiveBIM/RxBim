44961d7ec0bac9e2f737db0448a983d9cb63bef6
IgorSerdyukov <sis000@mail.ru>
2020-10-23 10:20:56 +0300

    [add]

---
+++ src/PikTools.Shared.RevitExtensions/Models/SharedParameterFileSource.cs
@@ -0,0 +1,13 @@
+﻿namespace PikTools.Shared.RevitExtensions.Models
+{
+    /// <summary>
+    /// Информация об источниках для файлов общих параметров
+    /// </summary>
+    public class SharedParameterFileSource
+    {
+        /// <summary>
+        /// Список путей к файлам общих параметров
+        /// </summary>
+        public string[] FilePaths { get; set; }
+    }
+}
