45f89e5e3e296980048f47a419252b74c83ec2f1
pavlovek <pavlovek@pik.ru>
2020-12-21 14:01:23 +0300

    add ModelFactory

---
+++ src/PikTools.Shared/Abstractions/IModelFactory.cs
@@ -0,0 +1,22 @@
+﻿namespace PikTools.Shared.Abstractions
+{
+    using System;
+
+    /// <summary>
+    /// Фабрика моделей
+    /// </summary>
+    public interface IModelFactory
+    {
+        /// <summary>
+        /// Создает экземпляр указанного типа, зарегистрированного в контейнере
+        /// </summary>
+        /// <typeparam name="T">Тип объекта</typeparam>
+        T GetInstance<T>();
+
+        /// <summary>
+        /// Создает экземпляр указанного типа, зарегистрированного в контейнере
+        /// </summary>
+        /// <param name="type">Тип объекта</param>
+        object GetInstance(Type type);
+    }
+}
---
+++ src/PikTools.Shared/ModelFactory.cs
@@ -0,0 +1,36 @@
+﻿namespace PikTools.Shared
+{
+    using System;
+    using PikTools.Shared.Abstractions;
+    using SimpleInjector;
+
+    /// <inheritdoc/>
+    public class ModelFactory : IModelFactory
+    {
+        private readonly Container _container;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ModelFactory"/> class.
+        /// </summary>
+        /// <param name="container">Конфигурация приложения</param>
+        public ModelFactory(Container container)
+        {
+            _container = container;
+        }
+
+        /// <inheritdoc />
+        public T GetInstance<T>()
+        {
+            var type = typeof(T);
+            var instance = _container.GetInstance(type);
+
+            return (T)instance;
+        }
+
+        /// <inheritdoc />
+        public object GetInstance(Type type)
+        {
+            return _container.GetInstance(type);
+        }
+    }
+}
