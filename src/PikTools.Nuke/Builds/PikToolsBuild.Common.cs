aa28aaf5a74b727db3d0dc9796e469ecdb48a801
Федор Жеков <zhekovfi@gmail.com>
2021-01-13 10:43:26 +0000

    Feature/#6139 nuke

---
+++ src/PikTools.Nuke/Builds/PikToolsBuild.Common.cs
@@ -0,0 +1,58 @@
+﻿#pragma warning disable SA1600, CS1591
+namespace PikTools.Nuke.Builds
+{
+    using System.Linq;
+    using global::Nuke.Common;
+    using global::Nuke.Common.IO;
+    using global::Nuke.Common.ProjectModel;
+    using global::Nuke.Common.Tools.DotNet;
+    using global::Nuke.Common.Utilities.Collections;
+    using static global::Nuke.Common.IO.PathConstruction;
+
+    public abstract partial class PikToolsBuild
+    {
+        public Target Clean => _ => _
+            .Description("Очищает решение")
+            .Executes(() =>
+            {
+                GlobDirectories(Solution.Directory, "**/bin", "**/obj")
+                    .Where(x => !IsDescendantPath(BuildProjectDirectory, x))
+                    .ForEach(FileSystemTasks.DeleteDirectory);
+            });
+
+        public Target Restore => _ => _
+            .Description("Восстанавливает пакеты")
+            .Requires(() => Project)
+            .DependsOn(Clean)
+            .Executes(() =>
+            {
+                DotNetTasks.DotNetRestore(s => s
+                    .SetProjectFile(GetProjectPath(Project)));
+            });
+
+        public Target Compile => _ => _
+            .Description("Собирает проект")
+            .Requires(() => Project)
+            .DependsOn(Restore)
+            .Executes(() =>
+            {
+                DotNetTasks.DotNetBuild(settings => settings
+                    .SetProjectFile(GetProjectPath(Project))
+                    .SetConfiguration(Configuration));
+            });
+
+        public Target Test => _ => _
+            .Description("Запускает тесты")
+            .Requires(() => Project)
+            .Executes(() =>
+            {
+                DotNetTasks.DotNetTest(settings => settings
+                    .SetProjectFile(GetProjectPath(Project)));
+            });
+
+        private AbsolutePath GetProjectPath(string name)
+        {
+            return Solution.AllProjects.FirstOrDefault(x => x.Name == name)?.Path ?? Solution.Path;
+        }
+    }
+}
---
+++ src/PikTools.Nuke/Builds/PikToolsBuild.cs
@@ -0,0 +1,103 @@
+﻿#pragma warning disable
+namespace PikTools.Nuke.Builds
+{
+    using System.IO;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+    using Generators;
+    using global::Nuke.Common;
+    using global::Nuke.Common.IO;
+    using global::Nuke.Common.Tools.Git;
+    using JetBrains.Annotations;
+
+    /// <summary>
+    /// Расширение билд скрипта для сборки MSI
+    /// </summary>
+    [PublicAPI]
+    public abstract partial class PikToolsBuild : NukeBuild
+    {
+        /// <summary>
+        /// ctor
+        /// </summary>
+        protected PikToolsBuild()
+        {
+            _wix = new Wix();
+        }
+
+        /// <summary>
+        /// BuildMsi
+        /// </summary>
+        public Target BuildMsi => _ => _
+            .Description("Собирает MSi пакет из указанного проекта")
+            .Requires(() => Project)
+            .Requires(() => Config)
+            .Executes(() =>
+            {
+                CreateOutDirectory();
+
+                _wix.BuildMsi(ProjectForMsiBuild,
+                    Solution.AllProjects,
+                    Config,
+                    (AbsolutePath)Cert,
+                    Password,
+                    Algorithm,
+                    ServerUrl);
+            });
+
+        /// <summary>
+        /// Собирает msi для тестирования
+        /// </summary>
+        public Target BuildMsiForTesting => _ => _
+            .Requires(() => Project)
+            .DependsOn(CheckStageVersion)
+            .Executes(() => { Config = "Debug"; })
+            .Triggers(BuildMsi);
+
+        /// <summary>
+        /// Собирает msi для тестирования
+        /// </summary>
+        public Target BuildMsiForProduction => _ => _
+            .Requires(() => Project)
+            .DependsOn(CheckProductionVersion)
+            .Executes(() => { Config = "Release"; })
+            .Triggers(BuildMsi);
+
+        /// <summary>
+        /// Собирает проект из тэга Testing{ProjectName}
+        /// </summary>
+        public Target BuildFromTag => _ => _
+            .Executes(() =>
+            {
+                CreateOutDirectory();
+
+                var regex = new Regex("Testing(?<projectName>.*)");
+                var projectsForBuild = GitTasks.Git("tag --points-at HEAD")
+                    .Select(x => x.Text)
+                    .Where(x => regex.IsMatch(x))
+                    .Select(x => regex.Match(x).Groups["projectName"].Value);
+
+                foreach (var projectName in projectsForBuild)
+                {
+                    var project = Solution.AllProjects.Single(x => x.Name == projectName);
+                    _wix.BuildMsi(project, Solution.AllProjects, "Debug");
+                }
+            });
+
+        /// <summary>
+        /// Генерирует необходимые свойства в проекте
+        /// </summary>
+        public Target GenerateProjectProps => _ => _
+            .Requires(() => Project)
+            .Requires(() => Config)
+            .Executes(() => new ProjectPropertiesGenerator().GenerateProperties(ProjectForMsiBuild, Config));
+
+        private void CreateOutDirectory()
+        {
+            var @out = Solution.Directory / "out";
+            if (!Directory.Exists(@out))
+            {
+                Directory.CreateDirectory(@out);
+            }
+        }
+    }
+}
