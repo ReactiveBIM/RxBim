6947bfebb21bd2caef0e5da80d7cfd04cb1771d6
pavlovek <pavlovek@pik.ru>
2021-04-15 15:53:31 +0300

    add TransactionService

---
+++ src/PikTools.Shared.RevitExtensions/Abstractions/ITransactionService.cs
@@ -0,0 +1,25 @@
+﻿namespace PikTools.Shared.RevitExtensions.Abstractions
+{
+    using System;
+    using CSharpFunctionalExtensions;
+
+    /// <summary>
+    /// Сервис по работе с транзакцией
+    /// </summary>
+    public interface ITransactionService
+    {
+        /// <summary>
+        /// Оборачивает действие в транзакцию
+        /// </summary>
+        /// <param name="action">Действие</param>
+        /// <param name="transactionName">Название транзакции</param>
+        Result MakeTransaction(Action action, string transactionName);
+
+        /// <summary>
+        /// Оборачивает действие в группу транзакций
+        /// </summary>
+        /// <param name="action">Действие</param>
+        /// <param name="transactionGroupName">Название группы транзакций</param>
+        Result MakeTransactionGroup(Action action, string transactionGroupName);
+    }
+}
