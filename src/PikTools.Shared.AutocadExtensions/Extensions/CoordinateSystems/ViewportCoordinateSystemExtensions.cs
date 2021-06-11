d13d76817e6c2408e88f3202ec5a8256e0e81207
Загорулькин Дмитрий <zagorulkindiu@pik.ru>
2021-06-11 17:17:00 +0300

    Point extensions

---
+++ src/PikTools.Shared.AutocadExtensions/Extensions/CoordinateSystems/ViewportCoordinateSystemExtensions.cs
@@ -0,0 +1,63 @@
+﻿namespace PikTools.Shared.AutocadExtensions.Extensions.CoordinateSystems
+{
+    using Autodesk.AutoCAD.DatabaseServices;
+    using Autodesk.AutoCAD.Geometry;
+    using JetBrains.Annotations;
+
+    /// <summary>
+    /// Расширения для видовых экранов для работы с системами координат
+    /// </summary>
+    [PublicAPI]
+    public static class ViewportCoordinateSystemExtensions
+    {
+         /// <summary>
+        /// Gets the transformation matrix from the specified paper space viewport Display Coordinate System (DCS)
+        /// to the paper space Display Coordinate System (PSDCS).
+        /// </summary>
+        /// <param name="vp">The instance to which this method applies.</param>
+        /// <returns>The DCS to PSDCS transformation matrix.</returns>
+        public static Matrix3d GetTransformMatrixFromDCSToPSDCS(this Viewport vp)
+        {
+            return
+                Matrix3d.Scaling(vp.CustomScale, vp.CenterPoint) *
+                Matrix3d.Displacement(vp.CenterPoint.GetAsVector()) *
+                Matrix3d.Displacement(vp.ViewCenter.ConvertTo3d().GetAsVector().Negate());
+        }
+
+        /// <summary>
+        /// Gets the transformation matrix from the specified model space viewport Display Coordinate System (DCS)
+        /// to the World Coordinate System (WCS).
+        /// </summary>
+        /// <param name="vp">The instance to which this method applies.</param>
+        /// <returns>The DCS to WCS transformation matrix.</returns>
+        public static Matrix3d GetTransformMatrixFromDCSToWCS(this Viewport vp)
+        {
+            return
+                Matrix3d.Rotation(-vp.TwistAngle, vp.ViewDirection, vp.ViewTarget) *
+                Matrix3d.Displacement(vp.ViewTarget - Point3d.Origin) *
+                Matrix3d.PlaneToWorld(vp.ViewDirection);
+        }
+
+        /// <summary>
+        /// Gets the transformation matrix from the Paper Space Display Coordinate System (PSDCS)
+        /// to the specified paper space viewport Display Coordinate System (DCS).
+        /// </summary>
+        /// <param name="vp">The instance to which this method applies.</param>
+        /// <returns>The PSDCS to DCS transformation matrix.</returns>
+        public static Matrix3d GetTransformMatrixFromPSDCSToDCS(this Viewport vp)
+        {
+            return vp.GetTransformMatrixFromDCSToPSDCS().Inverse();
+        }
+
+        /// <summary>
+        /// Gets the transformation matrix from the World Coordinate System (WCS)
+        /// to the specified model space viewport Display Coordinate System (DCS).
+        /// </summary>
+        /// <param name="vp">The instance to which this method applies.</param>
+        /// <returns>The WCS to DCS transformation matrix.</returns>
+        public static Matrix3d GetTransformMatrixFromWCSToDCS(this Viewport vp)
+        {
+            return vp.GetTransformMatrixFromDCSToWCS().Inverse();
+        }
+    }
+}
---
+++ src/PikTools.Shared.AutocadExtensions/Extensions/Point2dExtensions.cs
@@ -0,0 +1,32 @@
+﻿namespace PikTools.Shared.AutocadExtensions.Extensions
+{
+    using Autodesk.AutoCAD.Geometry;
+    using JetBrains.Annotations;
+
+    /// <summary>
+    /// Расширения для 2D точек
+    /// </summary>
+    [PublicAPI]
+    public static class Point2dExtensions
+    {
+        /// <summary>
+        /// Возвращает точку, смещённую относительно исходной
+        /// </summary>
+        /// <param name="basePoint">Исходная точка</param>
+        /// <param name="x">Смещение по X</param>
+        /// <param name="y">Смещение по Y</param>
+        public static Point2d OffsetPoint(this Point2d basePoint, double x, double y)
+        {
+            return basePoint + new Vector2d(x, y);
+        }
+
+        /// <summary>
+        /// Возвращает 3D точку с координатами X и Y исходной 2D точки и нулевой координатой Z
+        /// </summary>
+        /// <param name="point">Исходная 2D точка</param>
+        public static Point3d ConvertTo3d(this Point2d point)
+        {
+            return new Point3d(point.X, point.Y, 0d);
+        }
+    }
+}
