ab27b8f5f4278fc7b4525f09af49f915065c4ce1
Загорулькин Дмитрий <zagorulkindiu@pik.ru>
2021-06-17 15:35:18 +0300

    Get pik text style

---
+++ src/PikTools.Shared.AutocadExtensions/Extensions/DatabaseExtensions.cs
@@ -0,0 +1,33 @@
+﻿namespace PikTools.Shared.AutocadExtensions.Extensions
+{
+    using Autodesk.AutoCAD.DatabaseServices;
+
+    /// <summary>
+    /// Расширения для базы данных
+    /// </summary>
+    public static class DatabaseExtensions
+    {
+        /// <summary>
+        /// Возвращает идентификатор текстового стиля для ПИК.
+        /// Если стиля в чертеже нет, возвращается идентификатор текущего стиля.
+        /// </summary>
+        /// <param name="db">База данных чертежа</param>
+        public static ObjectId GetPikOrCurrentTextStyleId(this Database db)
+        {
+            const string pikTextStyleName = "PIK";
+            return db.GetTextStyleIdByNameOrCurrent(pikTextStyleName);
+        }
+
+        /// <summary>
+        /// Возвращает идентификатор текстового стиля по названию.
+        /// Если стиля с таким названием в чертеже нет, возвращается идентификатор текущего стиля.
+        /// </summary>
+        /// <param name="db">База данных</param>
+        /// <param name="textStyleName">Название текстового стиля</param>
+        private static ObjectId GetTextStyleIdByNameOrCurrent(this Database db, string textStyleName)
+        {
+            using var txtStylesTable = db.TextStyleTableId.OpenAs<TextStyleTable>();
+            return txtStylesTable.Has(textStyleName) ? txtStylesTable[textStyleName] : db.Textstyle;
+        }
+    }
+}
