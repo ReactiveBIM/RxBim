df927ac459e6fd05f66d1b66229346f04a233a6a
Konstantin Beder <kostya.bkb@mail.ru>
2021-06-29 14:54:40 +0000

    Feature/binding fix and transactions

--- src/PikTools.Shared.RevitExtensions/Abstractions/ITransactionService.cs
+++ src/PikTools.Shared.RevitExtensions/Abstractions/ITransactionService.cs
@@ -21,5 +21,19 @@
         /// <param name="action">Действие</param>
         /// <param name="transactionGroupName">Название группы транзакций</param>
         Result RunInTransactionGroup(Action action, string transactionGroupName);
+
+        /// <summary>
+        /// Оборачивает действие в транзакцию с возвратом результата
+        /// </summary>
+        /// <param name="action">Действие с возвратом результата</param>
+        /// <param name="transactionName">Название транзакции</param>
+        Result RunInTransaction(Func<Result> action, string transactionName);
+
+        /// <summary>
+        /// Оборачивает действие в группу транзакций с возвратом результата
+        /// </summary>
+        /// <param name="action">Действие с возвратом результата</param>
+        /// <param name="transactionGroupName">Название группы транзакций</param>
+        Result RunInTransactionGroup(Func<Result> action, string transactionGroupName);
     }
 }
--- src/PikTools.Shared.RevitExtensions/Services/TransactionService.cs
+++ src/PikTools.Shared.RevitExtensions/Services/TransactionService.cs
@@ -1,9 +1,9 @@
 ﻿namespace PikTools.Shared.RevitExtensions.Services
 {
     using System;
+    using Abstractions;
     using Autodesk.Revit.DB;
     using Autodesk.Revit.UI;
-    using PikTools.Shared.RevitExtensions.Abstractions;
     using Result = CSharpFunctionalExtensions.Result;
 
     /// <inheritdoc/>
@@ -25,47 +25,74 @@
         /// <inheritdoc/>
         public Result RunInTransaction(Action action, string transactionName)
         {
-            if (action == null)
-                return Result.Failure("Не задано действие для транзакции");
-
-            using var tr = new Transaction(CurrentDocument, transactionName);
-            try
+            Func<Result> funcWithResult = () =>
             {
-                tr.Start();
                 action.Invoke();
-                tr.Commit();
-            }
-            catch (Exception exeption)
-            {
-                if (tr.GetStatus() != TransactionStatus.RolledBack)
-                    tr.RollBack();
-                return Result.Failure(exeption.ToString());
-            }
+                return Result.Success();
+            };
 
-            return Result.Success();
+            return RunInTransaction(funcWithResult, transactionName);
         }
 
         /// <inheritdoc/>
         public Result RunInTransactionGroup(Action action, string transactionGroupName)
         {
+            Func<Result> funcWithResult = () =>
+            {
+                action.Invoke();
+                return Result.Success();
+            };
+
+            return RunInTransactionGroup(funcWithResult, transactionGroupName);
+        }
+
+        /// <inheritdoc />
+        public Result RunInTransaction(Func<Result> action, string transactionName)
+        {
             if (action == null)
                 return Result.Failure("Не задано действие для транзакции");
 
+            Result result;
+
+            using var tr = new Transaction(CurrentDocument, transactionName);
+            try
+            {
+                tr.Start();
+                result = action.Invoke();
+                tr.Commit();
+            }
+            catch (Exception exception)
+            {
+                if (tr.GetStatus() != TransactionStatus.RolledBack)
+                    tr.RollBack();
+                return Result.Failure(exception.ToString());
+            }
+
+            return result;
+        }
+
+        /// <inheritdoc />
+        public Result RunInTransactionGroup(Func<Result> action, string transactionGroupName)
+        {
+            if (action == null)
+                return Result.Failure("Не задано действие для транзакции");
+
+            Result result;
             using var tr = new TransactionGroup(CurrentDocument, transactionGroupName);
             try
             {
                 tr.Start();
-                action.Invoke();
+                result = action.Invoke();
                 tr.Assimilate();
             }
-            catch (Exception exeption)
+            catch (Exception exception)
             {
                 if (tr.GetStatus() != TransactionStatus.RolledBack)
                     tr.RollBack();
-                return Result.Failure(exeption.ToString());
+                return Result.Failure(exception.ToString());
             }
 
-            return Result.Success();
+            return result;
         }
     }
 }
