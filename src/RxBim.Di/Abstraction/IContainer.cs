3f64d3975188b6ebbe6622fce05e51c1731caf2a
Max Eroshkin <dealstone@gmail.com>
2021-09-09 13:44:57 +0300

    Feature/extract container interface (#2)
    * FX-T-17 Some IContainer methods moved to the ContainerExtensions.cs
    
    * FX-T-17 package version updated
    
    Co-authored-by: Max Eroshkin <max.a.eroshkin@gmail.com>

--- src/RxBim.Di/Abstraction/IContainer.cs
+++ src/RxBim.Di/Abstraction/IContainer.cs
@@ -2,184 +2,48 @@
 {
     using System;
     using System.Collections.Generic;
+    using JetBrains.Annotations;
 
     /// <summary>
-    /// DI container.
+    /// DI container abstraction.
     /// </summary>
+    [PublicAPI]
     public interface IContainer
     {
         /// <summary>
-        /// Adds a transient service of the type specified in <paramref name="serviceType"/> with an
+        /// Adds a service of the type specified in <paramref name="serviceType"/>
+        /// and the lifetime specified in <paramref name="lifetime"/> with an
         /// implementation of the type specified in <paramref name="implementationType"/> to this
         /// DI container.
         /// </summary>
         /// <param name="serviceType">The type of the service to register.</param>
         /// <param name="implementationType">The implementation type of the service.</param>
+        /// <param name="lifetime">The service lifetime.</param>
         /// <returns>A reference to this instance after the operation has completed.</returns>
-        IContainer AddTransient(Type serviceType, Type implementationType);
-        
-        /// <summary>
-        /// Adds a transient service of the type specified in <paramref name="serviceType"/> to this
-        /// DI container.
-        /// </summary>
-        /// <param name="serviceType">The type of the service to register and the implementation to use.</param>
-        /// <returns>A reference to this instance after the operation has completed.</returns>
-        IContainer AddTransient(Type serviceType);
+        IContainer Add(Type serviceType, Type implementationType, Lifetime lifetime);
 
         /// <summary>
-        /// Adds a transient service of the type specified in <typeparamref name="TService"/> with an
-        /// implementation type specified in <typeparamref name="TImplementation"/> to this
-        /// DI container.
-        /// </summary>
-        /// <typeparam name="TService">The type of the service to add.</typeparam>
-        /// <typeparam name="TImplementation">The type of the implementation to use.</typeparam>
-        /// <returns>A reference to this instance after the operation has completed.</returns>
-        IContainer AddTransient<TService, TImplementation>()
-            where TService : class
-            where TImplementation : class, TService;
-
-        /// <summary>
-        /// Adds a transient service of the type specified in <typeparamref name="TService"/> to this
-        /// DI container.
-        /// </summary>
-        /// <typeparam name="TService">The type of the service to add.</typeparam>
-        /// <returns>A reference to this instance after the operation has completed.</returns>
-        IContainer AddTransient<TService>()
-            where TService : class;
-
-        /// <summary>
-        /// Adds a transient service of the type specified in <typeparamref name="TService"/> using the
+        /// Adds a service of the type specified in <paramref name="serviceType"/>
+        /// and the lifetime specified in <paramref name="lifetime"/> using the
         /// factory specified in <paramref name="implementationFactory"/> to this
-        /// DI container.
-        /// </summary>
-        /// <typeparam name="TService">The type of the service to add.</typeparam>
-        /// <param name="implementationFactory">The factory that creates the service.</param>
-        /// <returns>A reference to this instance after the operation has completed.</returns>
-        IContainer AddTransient<TService>(Func<TService> implementationFactory)
-            where TService : class;
-
-        /// <summary>
-        /// Adds a scoped service of the type specified in <paramref name="serviceType"/> with an
-        /// implementation of the type specified in <paramref name="implementationType"/> to this
         /// DI container.
         /// </summary>
         /// <param name="serviceType">The type of the service to register.</param>
-        /// <param name="implementationType">The implementation type of the service.</param>
-        /// <returns>A reference to this instance after the operation has completed.</returns>
-        IContainer AddScoped(Type serviceType, Type implementationType);
-        
-        /// <summary>
-        /// Adds a scoped service of the type specified in <paramref name="serviceType"/> to this
-        /// DI container.
-        /// </summary>
-        /// <param name="serviceType">The type of the service to register and the implementation to use.</param>
-        /// <returns>A reference to this instance after the operation has completed.</returns>
-        IContainer AddScoped(Type serviceType);
-
-        /// <summary>
-        /// Adds a scoped service of the type specified in <typeparamref name="TService"/> with an
-        /// implementation type specified in <typeparamref name="TImplementation"/> to this
-        /// DI container.
-        /// </summary>
-        /// <typeparam name="TService">The type of the service to add.</typeparam>
-        /// <typeparam name="TImplementation">The type of the implementation to use.</typeparam>
-        /// <returns>A reference to this instance after the operation has completed.</returns>
-        IContainer AddScoped<TService, TImplementation>()
-            where TService : class
-            where TImplementation : class, TService;
-
-        /// <summary>
-        /// Adds a scoped service of the type specified in <typeparamref name="TService"/> to this
-        /// DI container.
-        /// </summary>
-        /// <typeparam name="TService">The type of the service to add.</typeparam>
-        /// <returns>A reference to this instance after the operation has completed.</returns>
-        IContainer AddScoped<TService>()
-            where TService : class;
-
-        /// <summary>
-        /// Adds a scoped service of the type specified in <typeparamref name="TService"/> using the
-        /// factory specified in <paramref name="implementationFactory"/> to this
-        /// DI container.
-        /// </summary>
-        /// <typeparam name="TService">The type of the service to add.</typeparam>
         /// <param name="implementationFactory">The factory that creates the service.</param>
+        /// <param name="lifetime">The service lifetime.</param>
         /// <returns>A reference to this instance after the operation has completed.</returns>
-        IContainer AddScoped<TService>(Func<TService> implementationFactory)
-            where TService : class;
+        IContainer Add(Type serviceType, Func<object> implementationFactory, Lifetime lifetime);
 
         /// <summary>
         /// Adds a singleton service of the type specified in <paramref name="serviceType"/> with an
-        /// implementation of the type specified in <paramref name="implementationType"/> to this
-        /// DI container.
-        /// </summary>
-        /// <param name="serviceType">The type of the service to register.</param>
-        /// <param name="implementationType">The implementation type of the service.</param>
-        /// <returns>A reference to this instance after the operation has completed.</returns>
-        IContainer AddSingleton(Type serviceType, Type implementationType);
-        
-        /// <summary>
-        /// Adds a singleton service of the type specified in <paramref name="serviceType"/> to this
-        /// DI container.
-        /// </summary>
-        /// <param name="serviceType">The type of the service to register and the implementation to use.</param>
-        /// <returns>A reference to this instance after the operation has completed.</returns>
-        IContainer AddSingleton(Type serviceType);
-
-        /// <summary>
-        /// Adds a singleton service of the type specified in <typeparamref name="TService"/> with an
-        /// implementation type specified in <typeparamref name="TImplementation"/> to this
-        /// DI container.
-        /// </summary>
-        /// <typeparam name="TService">The type of the service to add.</typeparam>
-        /// <typeparam name="TImplementation">The type of the implementation to use.</typeparam>
-        /// <returns>A reference to this instance after the operation has completed.</returns>
-        IContainer AddSingleton<TService, TImplementation>()
-            where TService : class
-            where TImplementation : class, TService;
-
-        /// <summary>
-        /// Adds a singleton service of the type specified in <typeparamref name="TService"/> to this
-        /// DI container.
-        /// </summary>
-        /// <typeparam name="TService">The type of the service to add.</typeparam>
-        /// <returns>A reference to this instance after the operation has completed.</returns>
-        IContainer AddSingleton<TService>()
-            where TService : class;
-
-        /// <summary>
-        /// Adds a singleton service of the type specified in <typeparamref name="TService" /> with an
         /// instance specified in <paramref name="implementationInstance"/> to this
         /// DI container.
         /// </summary>
+        /// <param name="serviceType">The type of the service to register.</param>
         /// <param name="implementationInstance">The instance of the service.</param>
         /// <returns>A reference to this instance after the operation has completed.</returns>
-        IContainer AddSingleton<TService>(TService implementationInstance)
-            where TService : class;
+        IContainer AddSingleton(Type serviceType, object implementationInstance);
 
-        /// <summary>
-        /// Adds a singleton service of the type specified in <typeparamref name="TService"/> using the
-        /// factory specified in <paramref name="implementationFactory"/> to this
-        /// DI container.
-        /// </summary>
-        /// <typeparam name="TService">The type of the service to add.</typeparam>
-        /// <param name="implementationFactory">The factory that creates the service.</param>
-        /// <returns>A reference to this instance after the operation has completed.</returns>
-        IContainer AddSingleton<TService>(Func<TService> implementationFactory)
-            where TService : class;
-
-        /// <summary>
-        /// Adds a single instance that will be returned when an instance of type
-        /// <typeparamref name="TService" /> is requested. This <paramref name="implementationInstance" /> must be thread-safe
-        /// when working in a multi-threaded environment.
-        /// <b>NOTE:</b> Do note that instances supplied by this method <b>NEVER</b> get disposed by the
-        /// container, since the instance is assumed to outlive this container instance.
-        /// </summary>
-        /// <param name="implementationInstance">The instance of the service.</param>
-        /// <returns>A reference to this instance after the operation has completed.</returns>
-        IContainer AddInstance<TService>(TService implementationInstance)
-            where TService : class;
-        
         /// <summary>
         /// Adds a single instance that will be returned when an instance of type
         /// <paramref name="serviceType" /> is requested. This <paramref name="implementationInstance" />
@@ -202,17 +66,6 @@
         IContainer Decorate(Type serviceType, Type decoratorType);
 
         /// <summary>
-        /// Adds a transient decorator of type <typeparamref name="TDecorator" /> for the service
-        /// of type <typeparamref name="TService"/>.
-        /// </summary>
-        /// <returns>A reference to this instance after the operation has completed.</returns>
-        /// <typeparam name="TService">The type of the service to decorate.</typeparam>
-        /// <typeparam name="TDecorator">The type of the decorator.</typeparam>
-        IContainer Decorate<TService, TDecorator>()
-            where TService : class
-            where TDecorator : class, TService;
-
-        /// <summary>
         /// Returns all type registrations.
         /// </summary>
         /// <returns></returns>
@@ -222,11 +75,5 @@
         /// <param name="serviceType">Type of object requested.</param>
         /// <returns>The requested service instance.</returns>
         object GetService(Type serviceType);
-
-        /// <summary>Gets an instance of the given <typeparamref name="TService" />.</summary>
-        /// <typeparam name="TService">Type of object requested.</typeparam>
-        /// <returns>The requested service instance.</returns>
-        TService GetService<TService>()
-            where TService : class;
     }
 }
