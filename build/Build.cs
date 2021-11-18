07bb4ae7c8891a040894bcf3952485682601322d
Max Eroshkin <max.a.eroshkin@gmail.com>
2021-11-18 13:33:01 +0300

    wip

--- build/Build.cs
+++ build/Build.cs
@@ -8,7 +8,9 @@ using Nuke.Common.CI.GitHubActions;
 using Nuke.Common.CI.SpaceAutomation;
 using Nuke.Common.Execution;
 using Nuke.Common.IO;
+using Nuke.Common.Tooling;
 using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.Git;
 using Nuke.Common.Utilities.Collections;
 using static Nuke.Common.IO.PathConstruction;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
@@ -19,7 +21,7 @@ using static Nuke.Common.Tools.DotNet.DotNetTasks;
     GitHubActionsImage.WindowsLatest,
     OnPushBranches = new[] { DevelopBranch },
     OnPullRequestBranches = new[] { DevelopBranch, "feature/**" },
-    InvokedTargets = new[] { nameof(Test), nameof(IPublish.Publish) },
+    InvokedTargets = new[] { nameof(IPublish.List), nameof(TagList) },
     ImportSecrets = new[] { "NUGET_API_KEY", "ALL_PACKAGES" })]
 [GitHubActions("Publish",
     GitHubActionsImage.WindowsLatest,
@@ -52,7 +54,6 @@ partial class Build : NukeBuild,
                 .Where(x => !IsDescendantPath(BuildProjectDirectory, x))
                 .ForEach(FileSystemTasks.DeleteDirectory);
         });
-    
 
     public Target Test => _ => _
         .Before(Clean)
@@ -63,7 +64,14 @@ partial class Build : NukeBuild,
                 .SetProjectFile(From<IHazSolution>().Solution.Path)
                 .SetConfiguration(From<IHazConfiguration>().Configuration));
         });
-    
+
+    public Target TagList => _ => _
+        .Executes(() =>
+        {
+            GitTasks.Git("tag --points-at HEAD").ToList().ForEach(x =>
+                Console.WriteLine($"tag: {x.Text}"));
+        });
+
     private T From<T>()
         where T : INukeBuild
         => (T)(object)this;
