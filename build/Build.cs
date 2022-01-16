d8f72028911a88fee38b4f9a12d92442787bbd1c
Zhekov Fedor <zhekovfi@gmail.com>
2022-01-16 13:50:56 +0300

    WIP

--- build/Build.cs
+++ build/Build.cs
@@ -1,11 +1,16 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Xml;
 using Bimlab.Nuke.Components;
+using ConsoleTableExt;
 using Nuke.Common;
 using Nuke.Common.CI.GitHubActions;
 using Nuke.Common.Execution;
 using Nuke.Common.IO;
+using Nuke.Common.Tooling;
 using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Utilities.Collections;
 using static Nuke.Common.IO.PathConstruction;
@@ -48,7 +53,6 @@ partial class Build : NukeBuild,
                 .Where(x => !IsDescendantPath(BuildProjectDirectory, x))
                 .ForEach(FileSystemTasks.DeleteDirectory);
         });
-    
 
     public Target Test => _ => _
         .Before(Clean)
@@ -57,9 +61,77 @@ partial class Build : NukeBuild,
         {
             DotNetTest(settings => settings
                 .SetProjectFile(From<IHazSolution>().Solution.Path)
-                .SetConfiguration(From<IHazConfiguration>().Configuration));
+                .SetConfiguration(From<IHazConfiguration>().Configuration)
+                .SetFilter("FullyQualifiedName!~IntegrationTests"));
         });
-    
+
+    Target IntegrationTests => _ => _
+        // .OnlyWhenDynamic(() => false)
+        .Executes(() =>
+        {
+            var solution = From<IHazSolution>().Solution;
+            //
+            var testProjectName = "RxBim.Example.IntegrationTests";
+            // var project = solution.AllProjects.FirstOrDefault(x => x.Name == testProjectName) ??
+            //               throw new ArgumentException("project not found");
+            //
+            var outputDirectory = solution.Directory / "testoutput";
+            // DotNetBuild(settings => settings
+            //     .SetProjectFile(project)
+            //     .SetConfiguration("Debug")
+            //     .SetOutputDirectory(outputDirectory));
+            //
+            var assemblyName = testProjectName + ".dll";
+            // var assemblyPath = outputDirectory / assemblyName;
+            //
+            var results = outputDirectory / "result.xml";
+            // RevitTestTasks.RevitTest(settings => settings
+            //     // .SetRevit(RevitVersions._2021)
+            //     .SetResults(results)
+            //     .SetDir(outputDirectory)
+            //     .SetProcessWorkingDirectory(outputDirectory)
+            //     .EnableContinuous()
+            //     .SetAssembly(assemblyPath));
+
+            var doc = new XmlDocument();
+            doc.LoadXml(File.ReadAllText(results));
+
+            var list = doc.DocumentElement.SelectNodes("/test-results/test-suite/results/test-suite");
+
+            foreach (XmlElement node in list)
+            {
+                var testFixtureName = node.Attributes["name"].Value;
+                var cases = node.FirstChild.ChildNodes;
+                var data = new List<List<object>>();
+                foreach (XmlElement @case in cases)
+                {
+                    var caseName = @case.Attributes["name"].Value;
+                    var isSuccess = @case.Attributes["success"].Value == "True";
+                    var executionTime = @case.Attributes["time"].Value;
+                    var failureMessage = isSuccess ? "-" : @case.FirstChild.FirstChild.InnerText + @case.FirstChild.LastChild.InnerText;
+                    data.Add(new List<object>
+                    {
+                        caseName,
+                        isSuccess ? "✔" : "❌",
+                        executionTime,
+                        failureMessage
+                    });
+                }
+
+                ConsoleTableBuilder.From(() => new ConsoleTableBaseData
+                    {
+                        Rows = data,
+                        Column = new List<object>
+                        {
+                            "Test name", "Result", "Execution time", "Failure message"
+                        }
+                    })
+                    .WithFormat(ConsoleTableBuilderFormat.Default)
+                    .WithTitle($"{assemblyName} - {testFixtureName} - ✔")
+                    .ExportAndWriteLine();
+            }
+        });
+
     private T From<T>()
         where T : INukeBuild
         => (T)(object)this;
