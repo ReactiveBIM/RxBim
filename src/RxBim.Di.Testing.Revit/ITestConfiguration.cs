89b4a0a02264764b4470ac2355fd000093797a32
Fedor Zhekov <zhekovfi@gmail.com>
2022-02-10 13:12:19 +0300

    Integrations tests target (#14)
    * added target for executing integration tests
    
    * warnings cleanup
    
    * fix CI
    
    * added test di configurator
    
    * fix namespace scope
    
    * WIP
    
    * added result converter
    
    * build fix
    
    * review fixes
    
    * version up
    
    * version up
    
    * review fixes
    
    * review fix
    
    * version up

---
+++ src/RxBim.Di.Testing.Revit/ITestConfiguration.cs
@@ -0,0 +1,7 @@
+namespace RxBim.Di.Testing.Revit
+{
+    /// <inheritdoc />
+    public interface ITestConfiguration : IPluginConfiguration
+    {
+    }
+}
---
+++ src/RxBim.Nuke.Revit/TestHelpers/Result.cshtml.cs
@@ -0,0 +1,9 @@
+﻿namespace RxBim.Nuke.Revit.TestHelpers
+{
+    /// <summary>
+    /// Result template
+    /// </summary>
+    public class Result
+    {
+    }
+}
