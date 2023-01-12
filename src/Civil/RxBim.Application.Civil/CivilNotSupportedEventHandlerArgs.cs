4ca150bf8e17fb3441f5f4d42b0d2ae50ddca7b2
zagorulkindiu <zagorulkindiu@pik.ru>
2023-01-12 14:04:27 +0300

    events wip

---
+++ src/Civil/RxBim.Application.Civil/CivilNotSupportedEventHandlerArgs.cs
@@ -0,0 +1,8 @@
+﻿namespace RxBim.Application.Civil;
+
+public class CivilNotSupportedEventHandlerArgs
+{
+    public string Message { get; set; }
+
+    public bool ShowMessage { get; set; } = true;
+}
--- src/Civil/RxBim.Application.Civil/RxBimApplication.cs
+++ src/Civil/RxBim.Application.Civil/RxBimApplication.cs
@@ -1,25 +1,44 @@
 ﻿namespace RxBim.Application.Civil
 {
+    using System;
     using Autocad.Base;
     using Autodesk.AutoCAD.AcInfoCenterConn;
     using Autodesk.Internal.InfoCenter;
     using Command.Civil;
 
+    public delegate void CivilNotSupportedEventHandler(object sender, CivilNotSupportedEventHandlerArgs args);
+
     /// <inheritdoc />
     public abstract class RxBimApplication : RxBimApplicationBase
     {
+        /// <summary>
+        /// Civil is not supported.
+        /// </summary>
+        public event CivilNotSupportedEventHandler? NotSupportedDetected;
+
         /// <inheritdoc />
         public override void Initialize()
         {
             if (!CivilUtils.IsCivilSupported())
             {
+                var args = new CivilNotSupportedEventHandlerArgs
+                {
+                    Message = $"Application {GetType().Assembly.GetName().Name} runs only in Civil 3D."
+                };
+
+                NotSupportedDetected?.Invoke(this, args);
+
+                if (!args.ShowMessage)
+                    return;
+
                 var resultItem = new ResultItem
                 {
-                    Title = $"Приложение {GetType().Assembly.GetName().Name} работает, только в Civil 3D.",
+                    Title = args.Message,
                     Type = ResultType.Error
                 };
 
                 new InfoCenterManager().PaletteManager.ShowBalloon(resultItem);
+
                 return;
             }
 
