b72c09b9b5b003d9573d9b6fc861d901f0563dc8
pavlovek <pavlovek@pik.ru>
2023-02-10 11:14:38 +0300

    fix inno builder

--- src/Core/RxBim.Nuke/Builders/InnoBuilder.cs
+++ src/Core/RxBim.Nuke/Builders/InnoBuilder.cs
@@ -101,7 +101,7 @@
         /// <param name="environment">Environment value.</param>
         public InnoBuilder AddRxBimEnvironment(string environment)
         {
-            Registry.CreateEntry(RegistryKeys.HKCU, @$"{Options.RxBimEnvironmentRegPath}\{{{_options.PackageGuid}}}")
+            Registry.CreateEntry(RegistryKeys.HKCU, @$"{Options.RxBimEnvironmentRegPath}\{{{{_options.PackageGuid}}}}")
                 .ValueName(Options.EnvironmentRegKeyName)
                 .ValueData(environment);
                 
