b55e7c7ac8c8cd51034efec674657f54ae6ceec9
Efrem <82603425+Snakye@users.noreply.github.com>
2025-06-23 10:30:26 +0300

    Поправил поиск файла PackageContents для получения переменной окружения (#94)
    * Начальный коммит
    
    * Фикс
    
    * Пакеты

--- src/Core/RxBim.Di/ConfigurationSources/EnvironmentConfigurationSource.cs
+++ src/Core/RxBim.Di/ConfigurationSources/EnvironmentConfigurationSource.cs
@@ -12,6 +12,7 @@
     internal class EnvironmentConfigurationSource : JsonConfigurationSource
     {
         private const string PackageContentsFileName = "PackageContents.xml";
+        private const int MaxRecursionLevel = 3;
 
         private readonly string _basePath;
         private readonly string _configFile;
@@ -19,7 +20,7 @@
         /// <summary>
         /// Initializes a new instance of the <see cref="EnvironmentConfigurationSource"/> class.
         /// </summary>
-        /// <param name="basePath">Base location.</param>
+        /// <param name="basePath">Base path.</param>
         /// <param name="configFile">Configuration JSON file.</param>
         public EnvironmentConfigurationSource(
             string basePath,
@@ -32,39 +33,50 @@
         /// <inheritdoc/>
         public override IConfigurationProvider Build(IConfigurationBuilder builder)
         {
-            var packageContentsFileDir = Directory.GetParent(_basePath);
-            var environment = EnvironmentRegistryConstants.DefaultEnvironment;
-
-            if (packageContentsFileDir is not null)
-            {
-                var packageContentsFile = System.IO.Path.Combine(
-                    packageContentsFileDir.FullName,
-                    PackageContentsFileName);
-
-                if (File.Exists(packageContentsFile))
-                {
-                    var xmlDoc = new XmlDocument();
-                    xmlDoc.Load(packageContentsFile);
-                    var productCode = xmlDoc.DocumentElement
-                        ?.SelectSingleNode("//ApplicationPackage")
-                        ?.Attributes?
-                        .GetNamedItem("ProductCode")
-                        .Value;
-
-                    if (!string.IsNullOrWhiteSpace(productCode))
-                    {
-                        environment = Registry.CurrentUser
-                            .OpenSubKey($"{EnvironmentRegistryConstants.RxBimEnvironmentRegPath}\\{productCode}")
-                            ?.GetValue(EnvironmentRegistryConstants.EnvironmentRegKeyName)
-                            ?.ToString() ?? EnvironmentRegistryConstants.DefaultEnvironment;
-                    }
-                }
-            }
-
+            var environment = GetEnvironment(_basePath);
             Path = $"{System.IO.Path.GetFileNameWithoutExtension(_configFile)}.{environment}.json";
             Optional = true;
 
             return base.Build(builder);
+        }
+
+        private string GetEnvironment(string basePath, int recursionLevel = 0)
+        {
+            if (recursionLevel == MaxRecursionLevel)
+                return EnvironmentRegistryConstants.DefaultEnvironment;
+
+            var packageContentsFileDir = Directory.GetParent(basePath);
+            var environment = EnvironmentRegistryConstants.DefaultEnvironment;
+
+            if (packageContentsFileDir is null)
+                return EnvironmentRegistryConstants.DefaultEnvironment;
+
+            var packageContentsFile = System.IO.Path.Combine(
+                packageContentsFileDir.FullName,
+                PackageContentsFileName);
+            if (!File.Exists(packageContentsFile))
+            {
+                recursionLevel++;
+                return GetEnvironment(packageContentsFileDir.FullName, recursionLevel);
+            }
+
+            var xmlDoc = new XmlDocument();
+            xmlDoc.Load(packageContentsFile);
+            var productCode = xmlDoc.DocumentElement
+                ?.SelectSingleNode("//ApplicationPackage")
+                ?.Attributes?
+                .GetNamedItem("ProductCode")
+                .Value;
+
+            if (!string.IsNullOrWhiteSpace(productCode))
+            {
+                environment = Registry.CurrentUser
+                    .OpenSubKey($"{EnvironmentRegistryConstants.RxBimEnvironmentRegPath}\\{productCode}")
+                    ?.GetValue(EnvironmentRegistryConstants.EnvironmentRegKeyName)
+                    ?.ToString() ?? EnvironmentRegistryConstants.DefaultEnvironment;
+            }
+
+            return environment;
         }
     }
 }
