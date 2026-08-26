a8175c0f92cfe92c8b857ec8c507f68716b60d1a
Vildar <khisyametdinovvt@pik.ru>
2026-08-26 19:27:26 +0400

    net8

---
+++ tests/RxBim.Di.Tests/Net8DependencyTests.cs
@@ -0,0 +1,34 @@
+﻿namespace RxBim.Di.Tests;
+
+using System.Linq;
+using FluentAssertions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyModel;
+using Microsoft.Extensions.Options;
+using Xunit;
+
+public class Net8DependencyTests
+{
+    private const int ExpectedMajorVersion = 8;
+
+    [Fact]
+    public void MicrosoftDependenciesShouldUseNet8Versions()
+    {
+        var assemblies = new[]
+        {
+            typeof(IConfiguration).Assembly,
+            typeof(ConfigurationBuilder).Assembly,
+            typeof(BinderOptions).Assembly,
+            typeof(JsonConfigurationSource).Assembly,
+            typeof(ServiceCollection).Assembly,
+            typeof(DependencyContext).Assembly,
+            typeof(Options).Assembly,
+            typeof(OptionsConfigurationServiceCollectionExtensions).Assembly,
+        };
+        var majorVersions = assemblies.Select(assembly => assembly.GetName().Version?.Major);
+
+        majorVersions.Should().OnlyContain(majorVersion => majorVersion == ExpectedMajorVersion);
+    }
+}
